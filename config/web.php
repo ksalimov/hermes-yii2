2767ab8fb5195145dd3e1b3737b6376687646aa2
ksalimov <kostiantyn.salimov@gmail.com>
2016-12-06 13:48:58 +0200

    enable Twig template engine

--- config/web.php
+++ config/web.php
@@ -46,6 +46,21 @@ $config = [
             ],
         ],
         */
+        'view' => [
+            'class' => 'yii\web\View',
+            'renderers' => [
+                'twig' => [
+                    'class' => 'yii\twig\ViewRenderer',
+                    'cachePath' => '@runtime/Twig/cache',
+                    //Array of twig options:
+                    'options' => [
+                        'auto_reload' => true,
+                    ],
+                    'globals' => ['html' => '\yii\helpers\Html'],
+                    'uses' => ['yii\bootstrap'],
+                ],
+            ],
+        ],
     ],
     'params' => $params,
 ];
